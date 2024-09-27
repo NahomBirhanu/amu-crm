bb4a8779b4685b36a941d1706ed4dfae200707e6
NahomBirhanu <nahom16birhanu@gmail.com>
2024-09-27 11:23:34 +0300

    browser router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import { App } from './App';
+// import { BrowserRouter } from 'react-router-dom';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter >
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import {App} from "./App";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById("root");
-render(<App />, root);
+render(<BrowserRouter>
+  <App />
+</BrowserRouter>, root);
